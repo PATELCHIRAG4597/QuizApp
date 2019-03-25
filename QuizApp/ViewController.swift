5fd9bc66477546a66e506ea40c63d163792bf148
Vivian Phung <VPhung11@gmail.com>
2019-03-24 20:02:02 -0400

    README update and Screenshots

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-// Outlet does not have any action or code run bc of it. Its can show something based on code though
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -19,8 +17,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var answer3: UIButton!
     @IBOutlet weak var progressLabel: UILabel!
     
-// Action does something when clicked
-
     @IBAction func submitAnswer0(_ sender: Any) {
         checkAnswer(idx: 0)
     }
@@ -37,7 +33,7 @@ class ViewController: UIViewController {
         checkAnswer(idx: 3)
     }
 
-    var currentQuestion: Question? // declares that its a question (that we will define later)
+    var currentQuestion: Question?
     var currentQuestionPosition: Int = 0
     var noCorrect: Int = 0
     
@@ -64,13 +60,11 @@ class ViewController: UIViewController {
             performSegue(withIdentifier: "sgShowResults", sender: nil)
         }
     }
-    // Before we move to the results screen pass the how many we got correct, and the total number of questions
     struct Question {
         let question: String
         let answers: [String]
         let correctAnswer: Int
     }
-    // struct does less than class. orginally there was only struc and then it evolved into classes.
     var questions: [Question] = [
         Question(
             question: "What is Taylor Swift's first album?",
@@ -96,19 +90,14 @@ class ViewController: UIViewController {
             question: "What is Taylor Swift's sixth album?",
             answers: ["1989", "Reputation", "Red", "Fearless"],
             correctAnswer: 1)
-        //More instances of Question should be inside this array.
-        //You need one instance of Question for each question in your quiz.
     ]
     
-    // this is the data we added
     
 
-    // viewdidload will always be there
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestion = questions[0]
         setQuestions() //this gives the value to the answers
-        // Do any additional setup after loading the view, typically from a nib.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "sgShowResults") {
